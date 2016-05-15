e2d26f4b9e06326331ce36298bfbcfbf1f005d84
AFedorov <aleksey.g.fedorov@gmail.com>
2016-05-15 10:04:36 +0300

    first

--- get_products_of_all_ints_except_at_index.nim
+++ get_products_of_all_ints_except_at_index.nim
@@ -1,1 +1,17 @@
-echo "hello"
+proc get_products_of_all_ints_except_at_index(int_list:  var openArray[int]): seq[int] =
+
+  result = @[int]
+  if int_list.len == 0:
+    return
+  if int_list.len == 1:
+    result = @int_list
+    return
+  # var 
+  for i in int_list.low..int_list.high:
+  	result.add( int_list[i])
+    echo ( int_list[i] )
+  echo( "Длина массива ", int_list.len, " элементов" )
+
+when isMainModule:
+  var int_list = @[1, 7, 3, 4]
+  echo ( @get_products_of_all_ints_except_at_index(int_list) )                                         
